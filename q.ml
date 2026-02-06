928bb55451d419e3bdd366d80d8e4bbd26386b77
manuelabla <manuellabellablanco@gmail.com>
2026-02-06 15:29:15 +0100

    Ejercicio 1 (b implementado (Parseo ya mejorado)

--- q.ml
+++ q.ml
@@ -1,23 +1,43 @@
-type dir =
-| Up  
-| Down
-
+type dir = Up | Down
+let rec mcd x y =                                             (*Función mcd para comprobar si*)
+  let rec aux a b =                                           (*una fracción es reducible    *)
+    if a = 0 || b = 0 then max a b
+    else aux (min a b) (a mod b)
+  in aux x y
+let n_pasos = ref 0                                           (*Referencia para el número de pasos*)
+let u = ref false                                             (*Referencia para flag unique*)
+let a = ref false                                             (*Referencia para flag -a*)
 let solve n =
   let rec aux (i, j, dir) count =                             (*Función de "movimiento el la matriz"*)
-    if count >= n then
-      () 
-    else match (i, j, dir) with  
-  | (1, _, Up) ->begin Printf.printf "%d/%d " i j ; 
-                 aux (i, j+1, Down) (count + 1)  end                  (*(1, _, subiendo) ⭢*)
-  | (_, 1, Down) ->begin Printf.printf "%d/%d " i j ;
-                   aux (i+1, j, Up) (count + 1) end                   (*(_, 1, bajando) ⭣*)
-  | (_, _, Up) ->begin Printf.printf "%d/%d " i j ; 
-                 aux (i-1, j+1, Up) (count + 1) end                   (*(_, _, subiendo) ↗*)
-  | (_, _, Down) ->begin Printf.printf "%d/%d " i j ;
-                   aux (i+1, j-1, Down) (count + 1) end               (*(_, _, bajando) ↘*)
-  in aux (1, 1, Up) 0                               
+    if count >= n then () 
+    else  match (i, j, dir) with  
+  | (1, _, Up) -> if !u && mcd (max i j) (min i j) > 1 
+                  then aux (i, j+1, Down) count 
+                  else begin Printf.printf "%d/%d " i j ;                   
+                  aux (i, j+1, Down) (count + 1) end          (*(1, _, subiendo) ⭢*)
+  | (_, 1, Down) -> if !u && mcd (max i j) (min i j) > 1 
+                    then aux (i+1, j, Up) count 
+                    else begin Printf.printf "%d/%d " i j ;
+                    aux (i+1, j, Up) (count + 1) end          (*(_, 1, bajando) ⭣*)
+  | (_, _, Up) -> if !u && mcd (max i j) (min i j) > 1 
+                  then aux (i-1, j+1, Up) count 
+                  else begin Printf.printf "%d/%d " i j ; 
+                  aux (i-1, j+1, Up) (count + 1) end          (*(_, _, subiendo) ↗*)
+  | (_, _, Down) -> if !u && mcd (max i j) (min i j) > 1 
+                    then aux (i+1, j-1, Down) count 
+                    else begin Printf.printf "%d/%d " i j ;
+                    aux (i+1, j-1, Down) (count + 1) end      (*(_, _, bajando) ↘*)
+  in aux (1, 1, Up) 0                           
   
-let parse =                                                 (*Parseador para el bash*)
-    if Array.length (Sys.argv) = 2 
-    then solve (int_of_string(Sys.argv.(1)))
-    else print_endline("Invalid number of arguments") ;
+let main =                                                   (*Función main*)
+ let speclist = [                                             (*Lista de flags*)
+  ("-a", Arg.Set a, "Activa modo a");
+  ("-u", Arg.Set u, "Activa modo unique");
+ ] in 
+ let usage_msg = "Uso: ./q [-a] [-u] <numero de pasos>" in    (*Mensaje de help para uso*)
+ let anon_fun s = n_pasos := int_of_string s in               (*Lectura de argumento*)
+ Arg.parse speclist anon_fun usage_msg;                       (*Parseo de los datos*)
+ if !n_pasos > 0 then begin 
+  solve !n_pasos;                                             (*Llamada a la función solve*)
+  print_newline()
+ end else Arg.usage speclist usage_msg                        (*Mensaje de error / ayuda*)
