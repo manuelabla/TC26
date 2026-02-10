65ec080393183a0df6a24b60fde95ed15a10e614
manuelabla <manuellabellablanco@gmail.com>
2026-02-10 12:15:07 +0100

    Comentarios del ejercicio 2 actualizados Persisten las dudas con el parseo de argumentos (string de ayuda)

--- P1/mg.ml
+++ P1/mg.ml
@@ -1,44 +1,44 @@
-let n_pasos = ref 0                                                       (*Referencia para el número de pasos*)
-let has_checked = ref false                                               (*Flag para comprobar si se ha hecho check*)      
+let n_pasos = ref 0                                                         (*Referencia para el número de pasos*)
+let has_checked = ref false                                                 (*Flag para comprobar si se ha hecho check*)      
 
-let string_builder x y =                                                  (*Función de construcción de strings*)
+let string_builder x y =                                                    (*Función de construcción de strings*)
   String.make x '~' ^ "m" ^
   String.make y '~' ^ "g" ^
   String.make (x + y) '~'
 
-let solve n =                                                             (*Función de printado de strings mg*)
+let solve n =                                                               (*Función de printado de strings mg*)
   let rec aux (x, y) accn =                                   
   if accn <= 0 then () 
   else match (x, y) with
-  |(_ , 1) -> begin Printf.printf"%s" (string_builder x y);               (*(_, 1) -> z + 1 y se empieza por (0, z)*)
+  |(_ , 1) -> begin Printf.printf"%s" (string_builder x y);                 (*(_, 1) -> z + 1 y se empieza por (0, z)*)
               print_newline();
               aux (0, x + y + 1) (accn - 1) end
-  |(_, _) -> begin Printf.printf"%s" (string_builder x y);                (*Resto de casos -> (x + 1, y - 1)*)
+  |(_, _) -> begin Printf.printf"%s" (string_builder x y);                  (*Resto de casos -> (x + 1, y - 1)*)
          print_newline();
          aux (x + 1, y - 1) (accn - 1) end 
-in aux (0, 1) n                                                           (*Caso Base*)
+in aux (0, 1) n                                                             (*Caso Base*)
 
-let check s =
-  let l = Str.split_delim(Str.regexp "[mg]") s in
+let check s =                                                               (*Función de checkeo de strings en el teorema mg*)
+  let l = Str.split_delim(Str.regexp "[mg]") s in                           (*Se parte el string en 3 (x,y y z) eliminando m y g y metiendolo en una lista*)
   match l with 
-  |[x; y; z] -> if String.length x >= 0 
-                && String.length x < String.length z 
-                && String.length y > 0 
-                && String.length y <= String.length z 
-                && String.length z == (String.length x + String.length y) 
-                then Printf.printf "yes" else Printf.printf "no"
-  | _ -> Printf.printf "no"
+  |[x; y; z] -> if String.length x >= 0                                     (*---CONDICIONES---*)
+                && String.length x < String.length z                        (*0 <= x < z*)
+                && String.length y > 0                                      (*0 < y <= z*)    
+                && String.length y <= String.length z                       (*z = x + y*)
+                && String.length z == (String.length x + String.length y)   
+                then Printf.printf "yes" else Printf.printf "no"            (*Si se cumple todo -> yes || No se cumple -> no*)
+  | _ -> Printf.printf "no"                                                 (*Resto de casos -> no*)
 
-  let main =                                                              (*Función main*)
- let usage_msg = "Uso: ./mg <numero de pasos | string xm yg z>"           (*Mensaje de help para uso*)
-in let anon_fun s =                                                       (*Lectura de argumento*)
+  let main =                                                                (*Función main*)
+ let usage_msg = "Uso: ./mg <numero de pasos | string xm yg z>"             (*Mensaje de help para uso*)
+in let anon_fun s =                                                         (*Lectura de argumento*)
   try n_pasos := int_of_string s 
  with Failure _ -> begin check s;
                    has_checked := true;
                    print_newline() end
-in Arg.parse [] anon_fun usage_msg;                                       (*Parseo de argumentos *)  
+in Arg.parse [] anon_fun usage_msg;                                         (*Parseo de argumentos *)  
   if !n_pasos > 0 then begin 
-    solve !n_pasos;                                                       (*Llamada a la función solve*)
+    solve !n_pasos;                                                         (*Llamada a la función solve*)
     print_newline ()
   end else if not !has_checked 
-           then print_endline usage_msg                                   (*Mensaje de error / ayuda*)
+           then print_endline usage_msg                                     (*Mensaje de error / ayuda*)
