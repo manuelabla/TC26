7f7bef65d7a5e8ac722a4a156a10708e9ca30ce7
manuelabla <manuellabellablanco@gmail.com>
2026-02-18 11:38:26 +0100

    Ejercicio 2 (b implementado perfectamente

--- P1/mg.ml
+++ P1/mg.ml
@@ -1,9 +1,13 @@
 let n_pasos = ref 0                                                         (*Referencia para el número de pasos*)
-let has_checked = ref false                                                 (*Flag para comprobar si se ha hecho check*)      
+let has_checked = ref false                                                 (*Flag para comprobar si se ha hecho check*)
+let permitidos = "mg~"
+
+let char_checker s =
+  String.for_all (fun c -> String.contains permitidos c) s                                                      
 
 let string_builder x y =                                                    (*Función de construcción de strings*)
   String.make x '~' ^ "m" ^
-  String.make y '~' ^ "g" ^
+  String.make y '~' ^ "g" ^ 
   String.make (x + y) '~'
 
 let solve n =                                                               (*Función de printado de strings mg*)
@@ -19,15 +23,23 @@ let solve n =                                                               (*Fu
 in aux (0, 1) n                                                             (*Caso Base*)
 
 let check s =                                                               (*Función de checkeo de strings en el teorema mg*)
+  if not (char_checker s) 
+  then Printf.printf "no \n"
+  else if not (String.contains s 'm') ||
+          not (String.contains s 'g') ||
+          not (String.contains s '~')
+  then Printf.printf "no \n"
+  else if String.index  s 'g' < String.index s 'm'
+  then Printf.printf "no \n" else                                                            
   let l = Str.split_delim(Str.regexp "[mg]") s in                           (*Se parte el string en 3 (x,y y z) eliminando m y g y metiendolo en una lista*)
   match l with 
-  |[x; y; z] -> if String.length x >= 0                                     (*---CONDICIONES---*)
+  |[x; y; z] -> if String.length x >= 0                                     (*---CONDICIONES de x, y y z---*)
                 && String.length x < String.length z                        (*0 <= x < z*)
                 && String.length y > 0                                      (*0 < y <= z*)    
                 && String.length y <= String.length z                       (*z = x + y*)
                 && String.length z == (String.length x + String.length y)   
-                then Printf.printf "yes" else Printf.printf "no"            (*Si se cumple todo -> yes || No se cumple -> no*)
-  | _ -> Printf.printf "no"                                                 (*Resto de casos -> no*)
+                then Printf.printf "yes \n" else Printf.printf "no \n"      (*Si se cumple todo -> yes || No se cumple -> no*)
+  | _ -> Printf.printf "no \n"                                              (*Resto de casos -> no*)
 
   let main =                                                                (*Función main*)
  let usage_msg = "Uso: ./mg <numero de pasos | string xm yg z>"             (*Mensaje de help para uso*)
